a856ddcd285f58f0253a8decde36f969a7f12fdb
Clement Jeulin <clement93jeulin@gmail.com>
2023-05-05 09:23:36 +0200

    Ajout de la fonction lower() aux answers afin que les réponses soient valides en min ou maj

--- index.py
+++ index.py
@@ -3,27 +3,27 @@ print("Welcome to my computer quiz :)")
 playing = input("Do you want to play ? ")
 print (playing)
 
-if playing != "yes":
+if playing.lower() != "yes":
         quit()
 
-print("Okay! Let's play :) Remember, all the answers have to be written in normal case")
+print("Okay! Let's play :) ")
 
 answer = input ("What does F.B.I stand for ? ")
-if answer == "federal bureau of investigation":
+if answer.lower() == "federal bureau of investigation":
         print("Correct !")
 else : print("INCORRECT !!!")
 
 answer = input ("What does B.A.C stand for ? ")
-if answer == "brigade anti criminalité":
+if answer.lower() == "brigade anti criminalité":
         print("Correct !")
 else : print("INCORRECT !!!")
 
 answer = input ("What does P.J stand for ? ")
-if answer == "police judiciaire":
+if answer.lower() == "police judiciaire":
         print("Correct !")
 else : print("INCORRECT !!!")
 
 answer = input ("What does U.F.C stand for ? ")
-if answer == "ultimate fighting championship":
+if answer.lower() == "ultimate fighting championship":
         print("Correct !")
 else : print("INCORRECT !!!")
