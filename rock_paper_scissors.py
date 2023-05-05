0e0ae550147b9c6ff137e1af07ddc83304964e73
Clement Jeulin <clement93jeulin@gmail.com>
2023-05-05 12:21:27 +0200

    Code cleaning

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -21,23 +21,19 @@ while True:
     if user_input == "rock" and computer_pick == "scissors": 
         print("You won !")
         user_wins += 1 
-        print("Your wins :", user_wins)
         continue
     elif user_input == "paper" and computer_pick == "rock": 
         print("You won !")
         user_wins += 1 
-        print("Your wins :", user_wins)
-
         continue
     elif user_input == "scissors" and computer_pick == "paper": 
         print("You won !")
         user_wins += 1 
-        print("Your wins :", user_wins)
         continue
 
     else:
         print("You Lost!")
         computer_wins += 1
-        print("Computer wins :", computer_wins)
 
+print("Computer won :", computer_wins, 'times.', "You won :", user_wins,"times.")
 print("Goodbye!")
