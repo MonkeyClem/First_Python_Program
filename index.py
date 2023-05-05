8c6ca6a4383a915cae7df4d4cd41bc8c8a0002f1
Clement Jeulin <clement93jeulin@gmail.com>
2023-05-05 09:11:01 +0200

    Mini-Quiz en python réalisé grâce aux conditions. Prochaine étape : Le programme doit se déclencher, que la réponse soit en majuscule ou minuscule

--- index.py
+++ index.py
@@ -1,11 +1,29 @@
-import pytube 
-from pytube import YouTube
+print("Welcome to my computer quiz :)")
 
-from sys import argv
-print(YouTube)
+playing = input("Do you want to play ? ")
+print (playing)
 
-livre = "Gatsby le Magnifique" 
-nourriture_preferee = "Glace"
-nouvelle_nourriture_preferee = "Pizza"
-nourriture_preferee = "Pâtes"
-print(nourriture_preferee)
+if playing != "yes":
+        quit()
+
+print("Okay! Let's play :) Remember, all the answers have to be written in normal case")
+
+answer = input ("What does F.B.I stand for ? ")
+if answer == "federal bureau of investigation":
+        print("Correct !")
+else : print("INCORRECT !!!")
+
+answer = input ("What does B.A.C stand for ? ")
+if answer == "brigade anti criminalité":
+        print("Correct !")
+else : print("INCORRECT !!!")
+
+answer = input ("What does P.J stand for ? ")
+if answer == "police judiciaire":
+        print("Correct !")
+else : print("INCORRECT !!!")
+
+answer = input ("What does U.F.C stand for ? ")
+if answer == "ultimate fighting championship":
+        print("Correct !")
+else : print("INCORRECT !!!")
