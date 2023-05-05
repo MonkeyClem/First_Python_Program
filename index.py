900deae0cd800bf0e7781c3f49733bb09d167599
Clement Jeulin <clement93jeulin@gmail.com>
2023-05-05 08:02:33 +0200

    installation et import de pytube

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
+import pytube 
+from pytube import YouTube
+
+from sys import argv
+print(YouTube)
+
 livre = "Gatsby le Magnifique" 
 nourriture_preferee = "Glace"
 nouvelle_nourriture_preferee = "Pizza"
